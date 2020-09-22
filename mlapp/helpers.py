7185bea29fc1a2f447f7b0496001358595175a96
AashishDhakal <aashish.dhakal@student.ku.edu.np>
2020-09-22 13:57:19 +0545

    started

---
+++ mlapp/helpers.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from django.conf import settings
+import numpy as np
+import cv2
+
+
+def process_image(img):
+    img_array = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (100, 100))
+    input_img = np.array(new_array)
+    input_img = input_img/255
+    input_img = input_img.reshape(-1,100,100,1)
+    return input_img
+
+
+def predict(img_array):
+    prediction_model = tf.keras.models.load_model(settings.MODEL_PATH)
+    prediction = prediction_model.predict(img)
+    result = np.argmax(prediction)
+    return result
+
---
+++ mlapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .helpers import predict, process_image
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
+
+# Create your views here.
+class PredictAPI(APIView):
+    parser_class = (FileUploadParser, )    
+
+    def post(self, request, *args, **kwargs):
+        image = request.POST.get('image', False)
+        array = process_image(image)
+        result = predict(array)
+        if result==0:
+            return Response({
+                'status': True,
+                'prediction': 'COVID 19'
+            })
+        elif result==1:
+            return Response({
+                'status': True,
+                'prediction': 'Normal'
+            })
+        elif result==2:
+            return Response({
+                'status': True,
+                'prediction': 'Viral Pneumonia'
+            })
+        else:
+            return Response({
+                'status': False,
+                'prediction': 'Unknown'
+            })
